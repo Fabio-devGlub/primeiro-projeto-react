a662642bbf8fa22222ac1fe2e88ef57d3efcae5b
Fabio-devGlub <Fabiobboy07@gmail.com>
2025-10-08 00:25:40 -0300

    altaração de espaco

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import GlobalStyles from './globalStyles.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  < GlobalStyles /> 
+  <GlobalStyles/> 
     <App />
   </StrictMode>,
 )
